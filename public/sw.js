0576d2c1fac3c93ac35987d9ff0a9bea2a6f7b76
Simon Darmandieu <simon.darmandieu@gmail.com>
2018-10-10 14:23:23 +0200

    heroku

--- public/sw.js
+++ public/sw.js
@@ -1,38 +1,21 @@
 var CACHE_NAME = 'itt';
-var urlsToCache = [
+var filesToCache = [
   'https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css',
   'https://code.jquery.com/jquery-3.3.1.slim.min.js',
   'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js',
   'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js'
 ];
 
-self.addEventListener('install', function(event) {
-  // Perform install steps
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
-  );
-});
-
-self.addEventListener('activate', function(event) {
-
-  var cacheWhitelist = ['itt'];
-
-  event.waitUntil(
-    caches.keys().then(function(cacheNames) {
-      return Promise.all(
-        cacheNames.map(function(cacheName) {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
+self.addEventListener('install', function(e) {
+  console.log('[ServiceWorker] Install');
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      console.log('[ServiceWorker] Caching app shell');
+      return cache.addAll(filesToCache);
     })
   );
 });
+
 
 self.addEventListener('fetch', function(event) {
   event.respondWith(
